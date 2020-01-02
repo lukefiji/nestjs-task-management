2cb09164276f7b212be1e6fd760af9535115243e
Luke Fiji <luke.fiji@gmail.com>
2020-01-02 12:38:04 -0800

    15 Create getAllTasks method

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,8 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
   // Dependency injection in NestJS is done within a class' constructor
   constructor(private tasksService: TasksService) {}
+
+  @Get()
+  async getAllTasks() {
+    return this.tasksService.getAllTasks();
+  }
 }
