5f2dcaa3a67de3b387d2221722122038886022a5
Luke Fiji <luke.fiji@gmail.com>
2020-01-10 19:49:41 -0800

    Set up JWT and Passport.js alongside their  NestJS wrappers

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,16 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
+    JwtModule.register({
+      secret: 'changeThisSecret123',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
     // Makes UserRepository available for injection throughout module
     TypeOrmModule.forFeature([UserRepository]),
   ],
