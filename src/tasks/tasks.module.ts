e4aa1dfcf0a8561f4b17155d10dfd7f24361e30f
Luke Fiji <luke.fiji@gmail.com>
2020-01-10 20:27:11 -0800

    Require login to retrieve all tasks

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -3,11 +3,13 @@ import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskRepository } from './task.repository';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
     // Makes TaskRepository instance injectable
     TypeOrmModule.forFeature([TaskRepository]),
+    AuthModule,
   ],
   controllers: [TasksController],
   providers: [TasksService],
