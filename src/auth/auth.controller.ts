7f593e877404adbece638e0eacc7301a3d1701cf
Luke Fiji <luke.fiji@gmail.com>
2020-01-10 20:24:54 -0800

    Remove /test route from auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,11 +6,8 @@ import {
   UseGuards,
   Req,
 } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthService } from './auth.service';
-import { GetUser } from './get-user.decorator';
-import { User } from './user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -28,11 +25,5 @@ export class AuthController {
     @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
   ): Promise<{ accessToken: string }> {
     return this.authService.signIn(authCredentialsDto);
-  }
-
-  @Post('/test')
-  @UseGuards(AuthGuard())
-  test(@GetUser() user: User) {
-    console.log(user);
   }
 }
