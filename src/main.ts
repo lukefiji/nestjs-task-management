579a16da99a3aeb03619052289fb5157348d13ef
Luke Fiji <luke.fiji@gmail.com>
2020-01-14 17:57:46 -0800

    Set up server env configuration

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
   const logger = new Logger('boostrap');
+  const app = await NestFactory.create(AppModule);
+
+  const serverConfig = config.get('server');
+  console.log(serverConfig);
+
   const port = 3000;
 
-  const app = await NestFactory.create(AppModule);
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
 }
